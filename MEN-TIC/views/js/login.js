fb16ddb80f0f2f9b7e0332dfbd1f49622d1a35f1
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2021-04-17 15:51:28 +0200

    Add files via upload

---
+++ MEN-TIC/views/js/login.js
@@ -0,0 +1,78 @@
+/*$( document ).ready(function() {
+    console.log( "ready!" );
+});*/
+$("#signup").click(function() {
+    $("#first").fadeOut("fast", function() {
+        $("#second").fadeIn("fast");
+    });
+});
+
+$("#signin").click(function() {
+    $("#second").fadeOut("fast", function() {
+        $("#first").fadeIn("fast");
+    });
+});
+
+
+
+$(function() {
+    $("form[name='login']").validate({
+        rules: {
+
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+
+            }
+        },
+        messages: {
+            email: "Por favor introduce un email valido",
+
+            password: {
+                required: "Por favor introduce la contraseña",
+
+            }
+
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+});
+
+
+
+$(function() {
+
+    $("form[name='registration']").validate({
+        rules: {
+            firstname: "required",
+            lastname: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 6
+            }
+        },
+
+        messages: {
+            firstname: "Por favor introduce tu nombre",
+            lastname: "Por favor introduce tu apellido",
+            password: {
+                required: "Por favor introduce una contraseña",
+                minlength: "Tu contraseña debe tener al menos 6 caracteres"
+            },
+            email: "Por favor introduce un email valido"
+        },
+
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+});
