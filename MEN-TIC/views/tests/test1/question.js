452435926898914926cdbaa74c15ae6c95dbbf79
pedro.fernandez2 <71818355+pedrofer95@users.noreply.github.com>
2021-06-05 01:29:41 +0200

    MIRAR TXT CAMBIOS
    En el TXT de cambios estan mencionados los archivos que he modificado desde mi útlima subida (la rama "Hasta Tema4")

---
+++ MEN-TIC/views/tests/test1/question.js
@@ -0,0 +1,32 @@
+let questions = [
+    {
+        numb: 1,
+        question: "¿Quién es el protagonista de 'El nombre del viento'?",
+        answer: "Kvothe",
+        options: ['Frolo', 'Lux', 'Arthas', 'Kvothe']
+    },
+    {
+        numb: 2,
+        question: "¿Qué tipo de Maiar es el mago Gandalf en el señor de los anillos?",
+        answer: "Un Istari",
+        options: ['Un Iluminado', 'Un Istari', 'Un dios', 'Uno de los antiguos']
+    },
+    {
+        numb: 3,
+        question: "¿Cúal es el apodo de Jaime Lannister?",
+        answer: "El Matarreyes",
+        options: ['El Matarreyes', 'El Coronado', 'Mano Dorada', 'El Zurdo']
+    },
+    {
+        numb: 4,
+        question: "¿Cúal es el nombre de la espada de Bilbo Bolson?",
+        answer: "Dardo",
+        options: ['Aguja', 'Dardo', 'Promesa', 'Excalibur']
+    },
+    {
+        numb: 5,
+        question: "¿A quién se enfrentará Thor en el Ragnarok?",
+        answer: "Jörmundgander",
+        options: ['Loki', 'Fenrir', 'Jörmundgander', 'Odin']
+    },
+];
