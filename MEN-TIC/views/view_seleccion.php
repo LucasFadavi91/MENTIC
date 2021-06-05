b05fbf5ae54c0bb862dd3e89c09daa279406888c
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2021-06-05 14:52:06 +0200

    Update view_seleccion.php

--- MEN-TIC/views/view_seleccion.php
+++ MEN-TIC/views/view_seleccion.php
@@ -1,16 +1,3 @@
-<?php 
-
-  session_start();
-
-    if (isset($_SESSION) && isset($_SESSION["id_user"]) === false) {
-      //exit("No estas logueado, datos incorrectos.");
-    //Redirecciono al login
-    header("location: ../controllers/controller_register_login.php");
-    exit;
-        
-    }
-?>
-
 <!doctype html>
 <html>
 <head>
