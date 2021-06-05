3707468115c71570bc1b1fe5ebaf6e2c21ab3067
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2021-06-05 17:51:26 +0200

    Add files via upload

---
+++ MEN-TIC/controllers/controller_preguntas.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("../models/model_register_login.php");
+
+if (!isset($_POST) || empty($_POST)) {
+
+    require_once("../views/view_preguntas.php");
+ 
+}else{
+
+
+}
+
+?>
---
+++ MEN-TIC/controllers/controllers_temas/controller_presentacion.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once("../../models/model_temas.php");
+
+
+if (!isset($_POST) || empty($_POST)) {
+
+    require_once("../../views/temario/view_presentacion.php");
+ 
+}else{
+
+
+}
+
+?>
