6764ba3af3d5898aa17f2c8a290774eac35431ca
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2021-06-08 13:57:52 +0200

    Add files via upload

---
+++ MEN-TIC/controllers/controllers_niveles/controller_modulo1_nivel3.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("../../models/models_modulos/model_modulo1.php");
+
+if (!isset($_POST) || empty($_POST)) {
+
+    require_once("../../views/views_niveles/view_modulo1_nivel3.php");
+ 
+}else{
+
+
+}
+
+?>
