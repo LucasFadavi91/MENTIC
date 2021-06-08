88de965c7a48dec991eb286171cf82195d582ece
Javier Gonzalez <71818412+Herhi@users.noreply.github.com>
2021-06-08 19:28:34 +0200

    Add files via upload

---
+++ MEN-TIC/views/tests/tests_subniveles_nivel1/test5/question.js
@@ -0,0 +1,100 @@
+let questions = [{
+        numb: 1,
+        question: "Señala los componentes conductuales que manifiesta David: ",
+        answer: "Dice que los chicos den centro ocupacional saben menos que él.",
+        options: ['Duerme mal.', 'Se pone nervioso al conocer gente nueva.', 'Se enfada y abandona la habitación.', 'Dice que los chicos den centro ocupacional saben menos que él.'
+        ]
+    },
+    {
+        numb: 2,
+        question: "¿Qué tipo de conductas manifiesta David?",
+        answer: "Conducta agresiva.",
+        options: ['Conducta adecuada.', 'Conducta estereotipada.', 'Conducta aprendida.', 'Conducta agresiva.']
+    },
+    {
+        numb: 3,
+        question: "Señala cuáles son estímulos psicológicos de la conducta de David: ",
+        answer: "Se pone nervioso al conocer gente nueva.",
+        options: ['Duerme mal.', 'Se pone nervioso al conocer gente nueva.', 'Se enfada.', 
+        'Dice que los chicos den centro ocupacional saben menos que él.']
+    },
+    {
+        numb: 4,
+        question: "La intervención que se llevaría a cabo con David en el centro ocupacional sería educativa o asistencial. Señala la que corresponda: ",
+        answer: "Intervención Educativa.",
+        options: ['Intervención directa.', 'Intervención Educativa.', 'Intervención Asistencial.', 'Todas son incorrectas.']
+    },
+    {
+        numb: 5,
+        question: "Qué estrategias pueden utilizar los padres de David para ayudarle a cambiar su actitud ante esta situación: ",
+        answer: "Todas son correctas.",
+        options: ['Acompañarle al lugar para que esté más tranquilo.', 'Practicar la ruta que debe tomar para llegar allí.', 'Repetir las veces que sea necesario hasta que se encuentre seguro.', 'Todas son correctas.']
+    },
+    {
+        numb: 6,
+        question: "¿Qué mecanismos de defensa está utilizando David para no enfrentarse a esa situación?",
+        answer: "Proyección.",
+        options: ['Negación.', 'Proyección.', 'Regresión.', 'Anticipación.']
+    },
+    {
+        numb: 7,
+        question: "David manifiesta rasgos de una personalidad psicológicamente sana puesto que es flexible a los cambios, tiene capacidad de afrontar situaciones nuevas y es tolerante (señala la correcta): ",
+        answer: "David tiene Síndrome de Down, por lo que no se le pueden aplicar esos criterios.",
+        options: ['David no manifiesta rasgos de una personalidad psicológicamente sana.', 'David manifiesta rasgos de una personalidad psicológicamente sana.'
+        , 'David tiene Síndrome de Down, por lo que no se le pueden aplicar esos criterios.', 'Todas son incorrectas.']
+    },
+    {
+        numb: 8,
+        question: "¿Qué función o funciones cognitivas podrá tener que utilizar David para que pueda llegar al centro ocupacional?",
+        answer: "Todas son correctas.",
+        options: ['Atención.', 'Memoria.', 'Pensamiento.', 'Todas son correctas.']
+    },
+    {
+        numb: 9,
+        question: "Para conseguir la máxima atención de David, debemos (elige la incorrecta): ",
+        answer: "Plantearlo de una manera sistematizada y muy estructurada.",
+        options: ['Que sea un momento en el que tenga sueño.', 'Hacerlo después de una actividad estresante.'
+        , 'Plantearlo de una manera sistematizada y muy estructurada.', 'No hacerlo antes de la hora de comer.']
+    },
+    {
+        numb: 10,
+        question: "Dentro de las dificultades de orientación que pueda presentar David, las más destacables serán: ",
+        answer: "Orientación espacial.",
+        options: ['Orientación espacial.', 'Orientación temporal.', 'Orientación personal.', 'Todas son correctas.']
+    },
+{
+        numb: 11,
+        question: "¿Cuál de las emociones básicas es la que está más presente en esta situación?",
+        answer: "Alegría.",
+        options: ['Alegría.', 'Tristeza.',
+            'Miedo.', 'Ira.']
+    },
+    {
+        numb: 12,
+        question: "¿Qué dificultad o dificultades puede presentar David para enfrentarse a esa situación?",
+        answer: "Todas son correctas.",
+        options: ['No recordar el recorrido que debe realizar.'
+        , 'No saber encontrar alternativas si se pierde.', 'Desorientación espacial en entornos desconocidos.'
+        , 'Todas son correctas.']
+    },
+    {
+        numb: 13,
+        question: "Para que David realice con éxito el trayecto hasta el centro ocupacional, la información adquirida: ",
+        answer: "Debe pasar a la memoria a largo plazo.",
+        options: ['Debe pasar a la memoria a largo plazo.', 'Debe ser pasar a la memoria a corto plazo.',
+            'Debe quedarse en el plano sensorial.', 'Todas son correctas.']
+    },
+    {
+        numb: 14,
+        question: "En relación con la siguiente afirmación: “Es necesario explicar todos los aspectos que David tiene que tener en cuenta al mismo tiempo, así podrá tener una visión global de todo y le será más fácil retener la información y almacenarla”, indica la opción incorrecta: ",
+        answer: "Explicarlo así le permite adaptarse a la vida real, es lo que se encontrará en todos los sitios.",
+        options: ['Es inadecuado pues se proporciona demasiada información junta.', 'Puede causar confusión y que se olvide pronto la información.', 'Explicarlo así no le permite poder procesar la información a su propio ritmo.', 
+        'Explicarlo así le permite adaptarse a la vida real, es lo que se encontrará en todos los sitios.']
+    },
+    {
+        numb: 15,
+        question: "¿Qué características de la dimensión social de David se aprecian en el caso práctico?",
+        answer: "Tiene una familia que le apoya.",
+        options: ['Tiene Síndrome de Down.', 'Tiene una familia que le apoya.', 'Se aprecian diferentes emociones.', 'Tiene un comportamiento agresivo debido a la inseguridad ante la nueva situación.']
+    },
+];
---
+++ MEN-TIC/views/views_niveles/view_modulo1_nivel3.php
@@ -0,0 +1,319 @@
+<?php 
+/*
+  session_start();
+
+  if (isset($_SESSION) && isset($_SESSION["id_user"]) === false) {
+    //exit("No estas logueado, datos incorrectos.");
+    //Redirecciono al login
+    header("location: ../controllers/controller_register_login.php");
+    exit;
+    
+  }
+  */
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset='utf-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<title>MENTIC - Nivel 3</title>
+<link href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' rel='stylesheet'>
+<link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css" integrity="sha256-ybRkN9dBjhcS2qrW1z+hfCxq+1aBdwyQM5wlQoQVt/0=" crossorigin="anonymous" />
+<link href='../../css/nightmode.css' rel='stylesheet'>
+<link rel="icon" href="../../img/favicon-16x16.png" type="image/png" sizes="16x16">
+
+<style>
+body {
+    background: whitesmoke;
+}
+
+/*Color Estrellas dificultad */
+.ratings i {
+    color: orangered;
+}
+
+.install span {
+    font-size: 12px
+}
+
+.col-md-4 {
+    margin-top: 27px
+}
+</style>
+    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+    <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js'></script>
+    <script type='text/javascript'></script>
+<link rel="stylesheet" href="../../css/tarjetas.css">
+</head>
+<body oncontextmenu='return false' class='snippet-body night-mode-available'>
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #007bff;">
+    <a class="navbar-brand" href="../../controllers/controller_menu_principal.php">
+      
+      <!-- Icono de Home -->
+      <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+        <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+      </svg>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown active">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Módulos
+          </a>
+          <div class="dropdown-menu night-mode-available" aria-labelledby="navbarDropdown">
+              <?php foreach($userModulos=getUserModulos($_SESSION["id_user"]) as $value) : ?>
+                  <a class="dropdown-item night-mode-available" href="../../controllers/controllers_modulos/controller_modulo<?php echo getNumeroModulo($value); ?>.php">
+                  <?php echo $value ?> </a>   
+              <?php endforeach; ?>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Más
+          </a>
+          <div class="dropdown-menu night-mode-available" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item night-mode-available" href="../../controllers/controller_temario.php">Temario</a>
+            <a class="dropdown-item night-mode-available" href="../controller_preguntas.php">Preguntas destacadas</a>    
+          </div>
+        </li>  
+      </ul>
+
+      <!-- ----------NIGHT MODE------------ -->
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <div class="night-mode-button">
+          <input type="checkbox" class="checkbox" id="night-mode">
+          <label for="night-mode" class="label">
+            <i class="fas fa-moon"></i>
+            <i class="fas fa-sun"></i>
+            <div class="blob"></div>
+          </label>
+        </div>
+      </li>
+    </ul>
+
+
+      <ul class="navbar-nav mr-right">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <?php echo htmlspecialchars($_SESSION["name"]) ?>
+          </a>
+          <div class="dropdown-menu night-mode-available" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item night-mode-available" href="../controller_perfil.php">Tu perfil</a>
+            <a class="dropdown-item night-mode-available" href="#">Configuración</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item night-mode-available" href="../../controllers/logout.php"><i class="fas fa-sign-out-alt"></i> Cerrar sesión</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">&nbsp;&nbsp;&nbsp;&nbsp;<?php echo getPuntos($_SESSION["id_user"]); ?> <i class="fa fa-star" style="color:yellow">&nbsp;&nbsp;&nbsp;&nbsp;</i></a>
+        </li>
+        
+      </ul>
+     
+      
+    </div>
+  </nav>
+  
+<!--  -------- EMPIEZAN LAS TARJETAS --------  -->
+
+
+<div class="container mt-5">
+    <div class="row night-mode-available">
+        <div class="col-md-4">
+            <div class="card p-3 effect night-mode-available">
+                <div class="d-flex flex-row mb-3"><img src="../../img/imagenes_subniveles/subnivel1.png" width="70" height="72">
+                    <div class="d-flex flex-column ml-2"><span>SUBNIVEL 1</span>
+                      <span class="text-black-50">Pasaba por aquí</span>
+                      <span class="ratings">
+                      <i class="fa fa-star"></i>
+                    </span></div>
+                </div>
+                <h6>Las personas estamos influenciadas por diferentes aspectos y/o dimensiones que conforman nuestra identidad biopsicosocial.</h6>
+                <div class="d-flex justify-content-between install mt-3">
+                <!-- CONTENIDO -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#subnivel1">Leer más</button>
+                  <a href="../../controllers/controllers_subniveles_nivel3/controller_modulo1_nivel3_subnivel1.php"><span class="text-primary">Realizar Test&nbsp;<i class="fa fa-angle-right"></i></span></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-3 effect night-mode-available">
+                <div class="d-flex flex-row mb-3"><img src="../../img/imagenes_subniveles/subnivel2.png" width="70" height="72">
+                    <div class="d-flex flex-column ml-2"><span>SUBNIVEL 2</span>
+                      <span class="text-black-50">Aficionado</span>
+                      <span class="ratings">
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                    </span></div>
+                </div>
+                <h6>Todas las personas tienen unas funciones cognitivas que les permiten y/o ayudan en su vida diaria, aunque en ocasiones no es así.</h6>
+                <div class="d-flex justify-content-between install mt-3">
+                <!-- CONTENIDO -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#subnivel2">Leer más</button>
+                  <a href="../../controllers/controllers_subniveles_nivel3/controller_modulo1_nivel3_subnivel2.php"><span class="text-primary">Realizar Test&nbsp;<i class="fa fa-angle-right"></i></span></div></a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-3 effect night-mode-available">
+                <div class="d-flex flex-row mb-3"><img src="../../img/imagenes_subniveles/subnivel3.png" width="70" height="72">
+                    <div class="d-flex flex-column ml-2"><span>SUBNIVEL 3</span>
+                      <span class="text-black-50">Especialista</span>
+                      <span class="ratings">
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                    </span></div>
+                </div>
+                <h6>Además de las funciones cognitivas básicas existentes otras funciones cognitivas que se consideran superiores debido a los procesos cognitivos.</h6>
+                <div class="d-flex justify-content-between install mt-3">
+                <!-- CONTENIDO -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#subnivel3">Leer más</button>
+                  <a href="../../controllers/controllers_subniveles_nivel3/controller_modulo1_nivel3_subnivel3.php"><span class="text-primary">Realizar Test&nbsp;<i class="fa fa-angle-right"></i></span></div></a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-3 effect night-mode-available">
+                <div class="d-flex flex-row mb-3"><img src="../../img/imagenes_subniveles/subnivel4.png" width="70" height="72">
+                    <div class="d-flex flex-column ml-2"><span>SUBNIVEL 4</span>
+                      <span class="text-black-50">Profesional</span>
+                      <span class="ratings">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                      </span></div>
+                </div>
+                <h6>Además de todo lo que hemos visto en los subniveles anteriores, hay varios aspectos que también son importantes de cara a conocer la dimensión biopsicosocial.</h6>
+                <div class="d-flex justify-content-between install mt-3">
+                <!-- CONTENIDO -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#subnivel4">Leer más</button>
+                  <a href="../../controllers/controllers_subniveles_nivel3/controller_modulo1_nivel3_subnivel4.php"><span class="text-primary">Realizar Test&nbsp;<i class="fa fa-angle-right"></i></span></div></a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-3 effect night-mode-available">
+                <div class="d-flex flex-row mb-3"><img src="../../img/imagenes_subniveles/subnivel5.png" width="70" height="72">
+                    <div class="d-flex flex-column ml-2"><span>SUBNIVEL 5</span>
+                      <span class="text-black-50">Experto</span>
+                      <span class="ratings">
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                    </span></div>
+                </div>
+                <h6>Una vez vistos todos los contenidos de la primera unidad de trabajo, vamos a comprobar los mismos aplicados a una situación práctica.</h6>
+                <div class="d-flex justify-content-between install mt-3">
+                <!-- CONTENIDO -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#subnivel5">Leer más</button>
+                  <a href="../../controllers/controllers_subniveles_nivel3/controller_modulo1_nivel3_subnivel5.php"><span class="text-primary">Realizar Test&nbsp;<i class="fa fa-angle-right"></i></span></div></a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-3 effect night-mode-available">
+                <div class="d-flex flex-row mb-3"><img src="https://st4.depositphotos.com/16418170/22329/v/1600/depositphotos_223291458-stock-illustration-ivan-turgenev-engraved-vector-portrait.jpg" width="70">
+                    <div class="d-flex flex-column ml-2"><span>Nivel 6</span>
+                      <span class="text-black-50">What is Lorem Ipsum?</span>
+                      <span class="ratings">
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                    </span></div>
+                </div>
+                <h6>What is Lorem Ipsum?What is Lorem Ipsum?What is Lorem Ipsum?What is Lorem Ipsum?</h6>
+                <div class="d-flex justify-content-between install mt-3"><span>What is Lorem Ipsum?</span><span class="text-primary">Realizar Test&nbsp;<i class="fa fa-angle-right"></i></span></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- MODAL SUBNIVEL1 -->
+<div id="subnivel1" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"> 
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content night-mode-available">
+      <div class="modal-header">
+        <h4 class="modal-title">SUBNIVEL 1</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="night-mode-available" aria-hidden="true">×</span></button>
+      </div> <br>
+      Cada persona tiene una forma diferente de aprender en la cual se encuentra más cómodo o cómoda en sus aprendizajes. Esto puede estar influenciada por las diferentes inteligencias que poseemos y por diferentes aspectos que pueden afectar a nuestra motivación y, por lo tanto, a la capacidad para aprender más y mejor.<br>
+      Es importante no juzgar y adaptarnos a las diferentes formas de aprender que tengan nuestros usuarios, de esa manera podemos motivarles y facilitar sus procesos, consiguiendo así mejores resultado.  ¡Vamos a ver ahora cómo te ha resultado de motivante este contenido!!!!<br>
+      ¡Adelante!
+    </div>
+  </div>
+</div>
+
+<!-- MODAL SUBNIVEL2 -->
+<div id="subnivel2" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"> 
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content night-mode-available">
+      <div class="modal-header">
+        <h4 class="modal-title">SUBNIVEL 2</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="night-mode-available" aria-hidden="true">×</span></button>
+      </div> <br>
+      Son muchas las teorías y/o corrientes que han intentado explicar a lo largo de la historia el proceso de aprendizaje de los seres humanos. Estas corrientes hacían referencia al medio, a la herencia o una mezcla de ambas como principales factores que podían favorecer o dificultar dicho aprendizaje.<br>
+      Vamos a ver algunas de las principales y que todavía siguen vigentes y se aplican en dichos procesos, ¿empezamos?
+    </div>
+  </div>
+</div>
+
+<!-- MODAL SUBNIVEL3 -->
+<div id="subnivel3" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"> 
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content night-mode-available">
+      <div class="modal-header">
+        <h4 class="modal-title">SUBNIVEL 3</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="night-mode-available" aria-hidden="true">×</span></button>
+      </div> <br>
+      Cada persona tiene una forma y un ritmo concreto de aprendizaje que hay que tener en cuenta. Eso no es diferente en las personas  con diversidad funcional o con discapacidad con las que trabajaremos en el futuro.<br>
+      Por eso es necesario conocer algunos de los aspectos de cada colectivo que pueden afectar al nivel de motivación y a las posibilidades de aprendizaje de estas personas. En concreto con las personas mayores y personas con diversidad funcional intelectual.<br>
+      Vamos a ver si tenemos claros estos contenidos. ¿Estás listo o lista para comprobarlo??
+    </div>
+  </div>
+</div>
+
+<!-- MODAL SUBNIVEL4 -->
+<div id="subnivel4" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"> 
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content night-mode-available">
+      <div class="modal-header">
+        <h4 class="modal-title">SUBNIVEL 4</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="night-mode-available" aria-hidden="true">×</span></button>
+      </div> <br>
+      Cada persona tiene una forma y un ritmo concreto de aprendizaje que hay que tener en cuenta. Eso no es diferente en las personas con diversidad funcional o con discapacidad con las que trabajaremos en el futuro.<br>
+      Por eso es necesario conocer algunos de los aspectos de cada colectivo que pueden afectar al nivel de motivación y a las posibilidades de aprendizaje de estas personas. En concreto con las personas con enfermedad mental, con demencias, con diversidad funcional física y sensorial y personas con enfermedad.<br>
+      ¡Te invito a comprobar lo claros que están estos contenidos!!
+    </div>
+  </div>
+</div>
+
+<!-- MODAL SUBNIVEL5 -->
+<div id="subnivel5" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"> 
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content night-mode-available">
+      <div class="modal-header">
+        <h4 class="modal-title">SUBNIVEL 5</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="night-mode-available" aria-hidden="true">×</span></button>
+      </div> <br>
+      Una vez vistos todos los contenidos de la tercera unidad de trabajo, vamos a comprobar los mismos aplicados a una situación práctica.<br>
+     	Para ello, se te plantea un caso práctico relacionado con dichos contenidos teóricos y se te formulará una serie de preguntas al respecto con la finalidad de relacionar los contenidos teóricos con o en la situación práctica. <br>
+       La mayor parte del camino ya la has realizado. Ahora sólo queda el último paso.<br>
+       ¿Preparado/a para darlo?<br>
+       ¡Vamos allá!
+    </div>
+  </div>
+</div>
+
+<script src="../../js/nightmode.js" type='text/javascript'></script>
+</body>
+</html>
