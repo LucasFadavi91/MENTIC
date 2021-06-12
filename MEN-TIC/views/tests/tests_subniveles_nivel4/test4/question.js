2fc368e64677617dffd6a04b496c2594929201df
Javier Gonzalez <71818412+Herhi@users.noreply.github.com>
2021-06-12 22:48:36 +0200

    Add files via upload

---
+++ MEN-TIC/views/tests/tests_subniveles_nivel4/test4/question.js
@@ -0,0 +1,129 @@
+let questions = [{
+        numb: 1,
+        question: "Las técnicas de modificación de conducta en las que se establece una negociación y un acuerdo entre profesional y usuario son: ",
+        answer: "Las dos respuestas anteriores son correctas.",
+        options: ['Contrato de conducta.', 'Economía de fichas.', 'Las dos respuestas anteriores son correctas.'
+        , 'Ninguna de  las respuestas es correcta.']
+    },
+    {
+        numb: 2,
+        question: "La Economía de Fichas es una técnica psicológica, muy utilizada con: ",
+        answer: "Los niños.",
+        options: ['Los niños.', 'Los mujeres.', 
+        'Los mayores.', 'Todas las respuestas anteriores son correctas.']
+    },
+    {
+        numb: 3,
+        question: "Completa la siguiente frase con el término que consideres acertado: “La economía de fichas se ha probado muy útil a la hora de fomentar conductas… que el usuario no realiza tanto como sería de esperar”: ",
+        answer: "Sociales.",
+        options: ['Correctas.',
+         'Adecuadas.',
+          'Sociales.', 'Todas las respuestas son correctas.']
+    },
+    {
+        numb: 4,
+        question: "Señala la afirmación correcta: ",
+        answer: "Se elegirá la técnica más adecuada en función de la edad y las características de la persona.",
+        options: ['La economía de fichas es más adecuada siempre que el contrato de conducta.',
+         'Se elegirá la técnica más adecuada en función de la edad y las características de la persona.',
+          'El contrato de conducta es siempre la mejor opción.',
+            'Ninguna de las respuestas anteriores es correcta.']
+    },
+    {
+        numb: 5,
+        question: "Al poner en práctica la economía de fichas definimos con el usuario: ",
+        answer: "Una serie de conductas deseables que son las que queremos fomentar.",
+        options: ['Una serie de conductas deseables que son las que queremos fomentar.'
+        , 'Una serie de conductas negativas.'
+        , 'Las dos respuestas anteriores son correctas.'
+        , 'Ninguna de las respuestas anteriores es correcta.']
+    },
+    {
+        numb: 6,
+        question: "Completa la siguiente frase con el término que consideres adecuado: “En la economía de fichas se utiliza  para premiar un….”: ",
+        answer: "Reforzador simbólico.",
+        options: ['Refuerzo positivo.', 'Reforzador simbólico.'
+        , 'Refuerzo negativo.',
+         'Reforzador concreto.'
+        ]
+    },
+    {
+        numb: 7,
+        question: "La materialización del contrato de conducta recoge: ",
+        answer: "Las conductas a realizar por los firmantes del contrato.",
+        options: ['Las conductas a realizar por los firmantes del contrato.',
+            'Utilizar señalizaciones y marcadores, ante el cambio de Los premios y castigos que se aplicarán antes de su cumplimiento.'
+            , 'Solo los premios que se aplicarán tras su incumplimiento.',
+            'Todas las respuestas son correctas.'
+        ]
+    },
+    {
+        numb: 8,
+        question: "En la economía de fichas se utilizan fichas que pueden ser canjeadas por un premio que puede ser: ",
+        answer: "Social o  material.",
+        options: ['Material  o comestible.',
+         'Social o  material.', 
+        'Social o comestible.',
+         'Material a corto plazo y material a largo plazo.'
+        ]
+    },
+    {
+        numb: 9,
+        question: "En la economía de fichas se podrán canjear las fichas por un premio cuando: ",
+        answer: "Cuando llegue al número de fichas establecido.",
+        options: ['Siempre que haga la conducta esperada.',
+         'Cuando llegue al número de fichas establecido.',
+          'Cuando el profesional decida.',
+            'Todas las respuestas son correctas.'
+        ]
+    },
+    {
+        numb: 10,
+        question: "A la técnica que combina el refuerzo positivo con el castigo negativo y el manejo del autocontrol, se denomina: ",
+        answer: "Economía de fichas.",
+        options: ['Sobrecorrección.',
+         'Castigo positivo.'
+         , 'Contrato de conducta.',
+          'Economía de fichas.']
+    },
+    {
+        numb: 11,
+        question: "En la economía de fichas, las fichas que se utilizan para aplicar esta técnica pueden ser: ",
+        answer: "Todas las respuestas son correctas.",
+        options: ['Cruces en un cuadro.'
+        , 'Marcas de rotulador.', 'Pegatinas.'
+        , 'Todas las respuestas son correctas.']
+    },
+    {
+        numb: 12,
+        question: "Completa la siguiente frase con el término que consideres correctos: “En la economía de fichas, el propio hecho de ir acumulando fichas es un…para el usuario”: ",
+        answer: "Refuerzo.",
+        options: ['Castigo.'
+        , 'Refuerzo.', 'Entretenimiento.', 'Estímulo.']
+    },
+    {
+        numb: 13,
+        question: "A la técnica en la que se llega a un acuerdo que se plasma en un documento generalmente escrito, se denomina: ",
+        answer: "Contrato de conducta.",
+        options: ['Contrato de conducta.', 'Economía de fichas.',
+            'Las dos respuestas son correctas.', 'Ninguna de las respuestas es correcta.']
+    },
+    {
+        numb: 14,
+        question: "Dentro de la economía de fichas, señala la incorrecta: ",
+        answer: "Se plantearán tareas que el adulto no quiera hacer para las hagan por él y aprendan valores de ayuda y colaboración.",
+        options: ['Se plantearán conductas relacionadas con actividades de cuidado y aseo personal.',
+         'Se plantearán conductas relacionadas con el cuidado y aseo del entorno.',
+         'Se plantearán tareas que el adulto no quiera hacer para las hagan por él y aprendan valores de ayuda y colaboración.', 
+        'Se plantearán conductas útiles y que se puedan usar con frecuencia.']
+    },
+    {
+        numb: 15,
+        question: "En relación al contrato de conducta, señala la opción incorrecta: ",
+        answer: "Se puede aplicar con cualquier tipo de persona, independientemente de su situación y grado de diversidad funcional.",
+        options: ['Sólo se puede aplicar con algunas personas.',
+        'Se puede aplicar con cualquier tipo de persona, independientemente de su situación y grado de diversidad funcional.',
+         'La persona debe estar de acuerdo con lo que se recoge en el contrato de conducta para que lo cumpla.',
+          'Los premios a conseguir deben ser del agrado de la persona para que resulten motivadores.']
+    },
+];
---
+++ MEN-TIC/views/tests/tests_subniveles_nivel4/test5/question.js
@@ -0,0 +1,124 @@
+let questions = [{
+        numb: 1,
+        question: "Para que una conducta positiva se repita es necesario: ",
+        answer: "Que se obtenga algo que me guste.",
+        options: ['Que se obtenga algo que me guste.', 'Que me quiten algo que me guste.', 'Que me den algo que no me guste.', 'Ninguna respuesta es correcta.'
+        ]
+    },
+    {
+        numb: 2,
+        question: "Francisco quiere vestirse sin ayuda, para ello Claudia puede: ",
+        answer: "Todas son correctas.",
+        options: ['Enseñarle la secuencia adecuada viendo un vídeo juntos.', 
+        'Utilizar imágenes donde aparezcan los pasos a seguir y el orden de los mismos.', 
+        'Empezar por el primer paso y repetirlo hasta que lo haya conseguido y hacer lo mismo con el resto hasta completar la secuencia.',
+         'Todas son correctas.']
+    },
+    {
+        numb: 3,
+        question: "¿Qué provocará en los profesionales y demás compañeros y compañeras la conducta de Francisco?",
+        answer: "Rechazo ante su comportamiento.",
+        options: ['Aceptación de su situación.', 'Indiferencia hacia lo que hace.', 'Rechazo ante su comportamiento.', 
+        'Todas las respuestas anteriores son falsas.']
+    },
+    {
+        numb: 4,
+        question: "Cada vez que Francisco tiene que hacer manualidades (que no le gustan) empieza a decir que no quiere hacerlo y está todo el rato quejándose. Claudia decide aplicar la extinción. Plantea la opción correcta: ",
+        answer: "No hace caso a lo que le dice y continua la actividad como “sí no pasara nada”.",
+        options: ['Habla con el para intentar convencerle.', 'Le saca un rato de la actividad hasta que se calma.',
+         'No hace caso a lo que le dice y continua la actividad como “sí no pasara nada”.',
+          'Claudia no puede hacer nada, es una decisión de Francisco.']
+    },
+    {
+        numb: 5,
+        question: "Cómo puede Claudia aplicar el refuerzo negativo con Francisco: ",
+        answer: "Le dirá que hoy no tiene que recoger los materiales si ha hecho algo que se esperaba y que no suele hacer.",
+        options: ['Le dará algo que le guste para reforzar esa conducta.', 
+        'Le dirá que hoy no tiene que recoger los materiales si ha hecho algo que se esperaba y que no suele hacer.'
+        , 'Le dará su comida favorita como premio a la buena conducta.', 'Todas las opciones son falsas.']
+    },
+    {
+        numb: 6,
+        question: "Claudia decide aplicar la técnica del tiempo fuera con Francisco, para ello deberá tener en cuenta: ",
+        answer: "Se le retira de la actividad durante un tiempo determinado sin estímulos que refuercen su conducta.",
+        options: ['Le aleja de los compañeros un tiempo, pero le permite seguir realizando la actividad.',
+         'Se le retira de la actividad durante un tiempo determinado sin estímulos que refuercen su conducta.',
+          'Se le permite continuar en la actividad y se le reprende una vez que ya ha terminado.', 
+          'Se refuerzan las conductas contrarias a la conducta que se quiere eliminar.']
+    },
+    {
+        numb: 7,
+        question: "Francisco no puede realizar la higiene de la misma manera que la realizaba antes. ¿Qué técnica se puede utilizar con él?",
+        answer: "Modelado.",
+        options: ['Modelado.', 'Castigo positivo.'
+        , 'Sobrecorrección.', 'Todas son correctas.']
+    },
+    {
+        numb: 8,
+        question: "Señala la opción incorrecta en relación al castigo positivo: ",
+        answer: "Se pueden utilizar en cualquier momento, pues siempre son efectivos.",
+        options: ['Se pueden utilizar en cualquier momento, pues siempre son efectivos.',
+         'Se aplican para corregir conductas agresivas.',
+          'Se pretende que la conducta inadecuada desaparezca.',
+           'Se da algo a la persona que le resulta desagradable para que no lo repita.']
+    },
+    {
+        numb: 9,
+        question: "Elige la opción correcta: “Francisco responde mal a un compañero y Claudia la verlo le responde igual de mal y le ridiculiza delante del resto de compañeros para que deje de hacer eso”: ",
+        answer: "El CI puede influir, pero también hay otros factores que determinarán la capacidad de aprendizaje de Antonio.",
+        options: ['Claudia conseguirá lo que se pretende pues está utilizando el castigo.', 'Claudia conseguirá que nadie en el centro haga eso nunca más.'
+        , 'Claudia no conseguirá que Francisco elimine esa conducta pues ha utilizado una estrategia inadecuada.',
+         'Todas las opciones son incorrectas.']
+    },
+    {
+        numb: 10,
+        question: "Claudia puede utilizar la técnica del modelado cuando quiera que Francisco aprenda conductas como: ",
+        answer: "Todas las opciones son correctas.",
+        options: ['Colaborar en tareas propias del centro de día.', 'Utilizar habilidades sociales básicas.'
+        , 'Mantener una conversación con otras personas.'
+        , 'Todas las opciones son correctas.']
+    },
+{
+        numb: 11,
+        question: "Claudia decide hacer una economía de fichas con Francisco. Señala la opción correcta: ",
+        answer: "Se utilizan reforzadores simbólicos como pegatinas que podrán canjear por los premios acordados.",
+        options: ['Siempre se utiliza con adultos pues resulta eficaz.'
+        , 'Combina el refuerzo positivo y el negativo, pero no los castigos.',
+            'Se utilizan reforzadores simbólicos como pegatinas que podrán canjear por los premios acordados.',
+             'Todas las respuestas son correctas.']
+    },
+    {
+        numb: 12,
+        question: "En un momento de enfado, Francisco tira un bote de pintura contra la pared. Claudia decide que ayudará a pintar de nuevo la pared para que quede como estaba antes. ¿Qué técnica está aplicando en esta ocasión?",
+        answer: "Sobrecorrección.",
+        options: ['Extinción.'
+        , 'Castigo positivo.', 'Refuerzo de conductas incompatibles.'
+        , 'Sobrecorrección.']
+    },
+    {
+        numb: 13,
+        question: "Esta mañana, Francisco ha recogido los materiales que estaba usando (cosa que no hace habitualmente sin que se lo tengas que repetir muchas veces). Claudia deberá aplicar en este caso: ",
+        answer: "Todas las opciones son correctas.",
+        options: ['Deberá comentarlo en voz alta para que todos se enteren, comparándolo con los demás.', 
+        'Le dará las gracias por haberlos colocado en su sitio.',
+            'Le invitará a cenar en un restaurante de lujo.', 
+            'Todas las opciones son correctas.']
+    },
+    {
+        numb: 14,
+        question: "Para que Claudia pueda utilizar la técnica del encadenamiento, no deberá: ",
+        answer: "Aumentar la exigencia a medida que se avance en el aprendizaje.",
+        options: ['Concretar la conducta que se quiere alcanzar y todas las subconductas necesarias.'
+        , 'Entrenar cada subconducta.', 
+        'Aumentar la exigencia a medida que se avance en el aprendizaje.', 
+        'Encadenar las subconductas para realizar la conducta completa.']
+    },
+    {
+        numb: 15,
+        question: "Claudia quiere que Francisco se comprometa por escrito a modificar algunas conductas, para ello podrá: ",
+        answer: "Todas las respuestas son correctas.",
+        options: ['Plantear un contrato de conducta.', 'Acordar las conductas que debe realizar.'
+        , 'Premios y castigos que se aplicarán.', 
+        'Todas las respuestas son correctas.']
+    },
+];
