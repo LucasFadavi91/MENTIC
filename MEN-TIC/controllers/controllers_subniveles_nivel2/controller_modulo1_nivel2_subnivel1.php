b9d4ddccc4c9923754b469aa724f135912b8522e
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2021-06-13 20:23:56 +0200

    Add files via upload

---
+++ MEN-TIC/controllers/controllers_subniveles_nivel2/controller_modulo1_nivel2_subnivel1.php
@@ -0,0 +1,30 @@
+<?php 
+
+  session_start();
+
+  if (isset($_SESSION) && isset($_SESSION["id_user"]) === false) {
+    //exit("No estas logueado, datos incorrectos.");
+    //Redirecciono al login
+    header("location: ../controllers/controller_register_login.php");
+    exit;
+    
+  }
+?>
+
+<?php
+
+
+require_once("../../models/models_modulos/model_modulo1.php");
+
+$puntosModulo=getPuntos($_SESSION["id_user"]);
+
+
+
+
+header("location: ../../views/tests/tests_subniveles_nivel2/test1/index.html");
+
+
+
+
+
+?>
