9841db3648ca9e12e7b10192b90d154b09a0931b
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2021-06-13 20:26:10 +0200

    Add files via upload

---
+++ MEN-TIC/views/tests/tests_subniveles_nivel3/test1/question.js
@@ -0,0 +1,107 @@
+let questions = [{
+        numb: 1,
+        question: "¿A través de qué proceso se adapta el ser humano al medio?",
+        answer: "Aprendizaje.",
+        options: ['Motivación.', 'Aprendizaje.', 'Creatividad.',
+            'Las tres respuestas anteriores son correctas.']
+    },
+    {
+        numb: 2,
+        question: "De las siguientes afirmaciones, ¿cuál es la correcta: ",
+        answer: "La mayoría de nuestras habilidades son aprendidas.",
+        options: ['Todas nuestras habilidades son heredadas.', 'La mayoría de nuestras habilidades son aprendidas.', 'El 90% de nuestras habilidades son heredadas y el resto aprendidas.'
+        , 'El 50% de nuestras habilidades son heredadas. y el otro 50% aprendidas.']
+    },
+    {
+        numb: 3,
+        question: "¿Qué es necesario para que una persona pueda aprender?",
+        answer: "Que exista motivación y la oportunidad de ejecutar la conducta aprendida.",
+        options: ['Que cuente con buenos profesores y buenas capacidades cognitivas.', 'Que la persona tenga motivación  y buenas capacidades memorísticas.', 'Que exista motivación y la oportunidad de ejecutar la conducta aprendida.'
+        , 'Que repita las conductas  enseñadas muchas veces.']
+    },
+    {
+        numb: 4,
+        question: "Relacionada  con la habilidad de aprender, elige la afirmación más correcta: ",
+        answer: "Una persona más inteligente aprende más rápido y aplica los aprendizajes a diferentes situaciones.",
+        options: ['Una persona más madura aprende más rápido y aplica los aprendizajes a diferentes situaciones.'
+        , 'Una persona más inteligente aprende más rápido y aplica los aprendizajes a diferentes situaciones.'
+        , 'Una persona más motivada  aprende más rápido y aplica los aprendizajes a diferentes situaciones.'
+        , 'Una persona con una actitud positiva  aprende más rápido y aplica los aprendizajes a diferentes situaciones.']
+    },
+    {
+        numb: 5,
+        question: "A la capacidad de la persona para adaptarse y enfrentar situaciones nuevas de forma ágil, sin que el aprendizaje previo, la experiencia o el conocimiento adquirido supongan una ayuda determinante para su manifestación. , se denomina: ",
+        answer: "Inteligencia fluida.",
+        options: ['Inteligencia cristalizada.', 'Inteligencia fluida.', 'Inteligencia interpersonal.', 
+        'Inteligencia cognitiva.']
+    },
+    {
+        numb: 6,
+        question: "Señala la palabra que completaría la siguiente afirmación: El… deteriora las estructuras cerebrales y, por lo tanto, afecta a las funciones cognitivas como la memoria o la orientación espacial: ",
+        answer: "Estrés.",
+        options: ['Sueño.','Sed.', 'Estrés.', 'Desmotivación.']
+    },
+    {
+        numb: 7,
+        question: "Completa la frase: “Si EM es < EC, la inteligencia será… al promedio",
+        answer: "Superior.",
+        options: ['Superior.', 'Inferior.', 'Similar.', 'No es significativa esta diferencia.']
+    },
+    {
+        numb: 8,
+        question: "De las siguientes afirmaciones señala aquella que corresponden a la inteligencia cristalizada: ",
+        answer: "Se refiere a la utilización de las habilidades intelectuales ya aprendidas y depende mucho del aprendizaje obtenido de la experiencia de una persona en el contexto cultural en que vive y se relaciona.",
+        options: ['Se refiere a la utilización de las habilidades intelectuales ya aprendidas y depende mucho del aprendizaje obtenido de la experiencia de una persona en el contexto cultural en que vive y se relaciona.',
+         'Es la capacidad que permite adquirir nuevos conocimientos y adaptarse a situaciones nuevas.', 'Permite resolver analogías, agrupar números, ordenar conjuntos,…etc.', 
+         'Todas las respuestas son correctas.']
+    },
+    {
+        numb: 9,
+        question: "La teoría de las inteligencias múltiples es un modelo  en el que: ",
+        answer: "Todas las afirmaciones anteriores son correctas.",
+        options: ['La inteligencia no es vista como algo unitario que agrupa diferentes capacidades específicas, sino como un conjunto de inteligencias múltiples, distintas e independientes.', 
+        'Define la inteligencia como la «capacidad de resolver problemas y/o elaborar productos que sean valiosos en una o más culturas.'
+        , 'Añade que, así como hay muchos tipos de problemas que resolver, también hay muchos tipos de inteligencia.', 'Todas las afirmaciones anteriores son correctas.']
+    },
+    {
+        numb: 10,
+        question: "¿Qué inteligencia permite expresar sentimientos, emociones, opiniones, ideas y  conocimientos?",
+        answer: "Inteligencia lingüístico – verbal.",
+        options: ['Inteligencia lógico-matemática.', 'Inteligencia intrapersonal.', 'Inteligencia musical.', 'Inteligencia lingüístico – verbal.']
+    },
+    {
+        numb: 11,
+        question: "A la predisposición a responder de una determinada manera frente a una situación se denomina: ",
+        answer: "Experiencia.",
+        options: ['Inteligencia.', 'Experiencia.', 
+        'Actitud, pensamos que no servimos.', 'Motivación.']
+    },
+    {
+        numb: 12,
+        question: "Cuando utilizamos el propio cuerpo para realizar actividades o resolver problemas que requieren fuerzas, rapidez, coordinación, flexibilidad, equilibrio,… etc. estamos utilizando: ",
+        answer: "Ninguna de las anteriores es correcta.",
+        options: ['Inteligencia visual – espacial.', 'Inteligencia naturalista.', 'Inteligencia musical.', 'Ninguna de las anteriores es correcta.']
+    },
+    {
+        numb: 13,
+        question: "De las siguientes afirmaciones, señala la falsa: ",
+        answer: "La primera y tercera  respuesta son correctas.",
+        options: ['Detrás de cada deseo siempre hay una motivación.'
+        , 'Las recompensas y los premios no pueden llevarnos a aprender nuevas conductas ni a motivar más a una persona.'
+        , 'Motivación y aprendizaje están unidos, pues del grado de motivación va a depender el aprendizaje de pautas de comportamiento o la adquisición de nuevas conductas..'
+        , 'La primera y tercera  respuesta son correctas.']
+    },
+    {
+        numb: 14,
+        question: "Al conjunto de experiencias que la persona posee y que le serán útiles para afrontar nuevos conocimientos se denomina: ",
+        answer: "Todas son correctas.",
+        options: ['Psicología evolutiva y social.', 'Psicología educativa y del aprendizaje.', 'Psicología industrial.', 'Todas son correctas.']
+    },
+    {
+        numb: 15,
+        question: "Señala la afirmación que corresponde con un estímulo cognitivo. ",
+        answer: "Experiencia.",
+        options: ['Motivación.', 
+        'Experiencia.', 'Madurez.', 'Inteligencia.']
+    },
+];
---
+++ MEN-TIC/views/tests/tests_subniveles_nivel5/test5/question.js
@@ -0,0 +1,128 @@
+let questions = [{
+        numb: 1,
+        question: "¿En qué colectivo podemos ubicar a Martina?",
+        answer: "Personas mayores.",
+        options: ['Personas con demencia.', 
+        'Personas mayores.', 
+        'Personas con enfermedad mental.', 
+        'Ninguna de las opciones es correcta.'
+        ]
+    },
+    {
+        numb: 2,
+        question: "¿En qué colectivo podemos ubicar a Lucila?",
+        answer: "Personas con demencia.",
+        options: ['Personas mayores.', 
+        'Personas con demencia.', 
+        'Personas con discapacidad intelectual.',
+         'Ninguna de las opciones es correcta.']
+    },
+    {
+        numb: 3,
+        question: "¿Qué técnica será la más adecuada con Martina?",
+        answer: "Entrenamiento de la memoria.",
+        options: ['Terapia de orientación a la realidad.', 'Entrenamiento de la memoria.', 'Reminiscencia.', 
+        'Todas son técnicas que se deben aplicar con Martina.']
+    },
+    {
+        numb: 4,
+        question: "¿Qué técnica será la más adecuada con Lucila?",
+        answer: "Todas son técnicas que se pueden aplicar con Lucila.",
+        options: ['Terapia de orientación a la realidad.', 'Entrenamiento de la memoria.',
+         'Reminiscencia.',
+          'Todas son técnicas que se pueden aplicar con Lucila.']
+    },
+    {
+        numb: 5,
+        question: "¿Qué función cognitiva tienen afectada en mayor medida Martina y Lucila?",
+        answer: "Razonamiento.",
+        options: ['Atención.', 
+        'Percepción.'
+        , 'Memoria.', 'Razonamiento.']
+    },
+    {
+        numb: 6,
+        question: "Una actividad de psicomotricidad fina que podemos realizar con Martina será: ",
+        answer: "Unir con líneas para hacer un dibujo.",
+        options: ['Levantar los brazos y ponerlos a la altura de la cabeza.',
+         'Pasarse un balón.',
+          'Unir con líneas para hacer un dibujo.', 
+          'Subir y bajar escalones.']
+    },
+    {
+        numb: 7,
+        question: "Son materiales que se pueden utilizar para trabajar la orientación a la realidad: ",
+        answer: "Todas las opciones son correctas.",
+        options: ['Calendarios de actividades.', 'Símbolos en las puertas.'
+        , 'Líneas en el suelo para marcar los recorridos.', 'Todas las opciones son correctas.']
+    },
+    {
+        numb: 8,
+        question: "Son actividades que podemos utilizar para trabajar la atención: ",
+        answer: "Realización de mandalas y puzles.",
+        options: ['Realización de mandalas y puzles.',
+         'Consultar frecuentemente el reloj.',
+          'Reconocer objetos por el tacto.',
+           'Todas las respuestas anteriores son correctas.']
+    },
+    {
+        numb: 9,
+        question: "Qué características presentan las personas con demencia en relación al lenguaje?",
+        answer: "Presentan alteraciones sintácticas.",
+        options: ['Hablan mucho y de manera descoordinada.', 
+        'Hablan poco, pero cuando hablan lo hacen muy bien.'
+        , 'Presentan alteraciones sintácticas.',
+         'Todas las opciones son correctas.']
+    },
+    {
+        numb: 10,
+        question: "¿Qué beneficios puede suponer la musicoterapia para Lucila?",
+        answer: "Todas las respuestas anteriores son correctas.",
+        options: ['Mejora de las condiciones físicas.', 'Mejora del estado de ánimo.'
+        , 'Mejora de las condiciones cognitivas.'
+        , 'Todas las respuestas anteriores son correctas.']
+    },
+{
+        numb: 11,
+        question: "Sí quisiéramos utilizar la categorización con las usuarias, está consistiría en: ",
+        answer: "Ordenar los conceptos agrupados por aspectos en común.",
+        options: ['Repetir mentalmente lo que queremos aprender varias veces.'
+        , 'Ordenar los conceptos agrupados por aspectos en común.',
+            'Unir una información nueva con otra más conocida.',
+             'Identificar un contenido a recordar con imágenes.']
+    },
+    {
+        numb: 12,
+        question: "¿Qué actividades podemos plantear para trabajar el lenguaje con las usuarias?",
+        answer: "Todas las respuestas anteriores son correctas.",
+        options: ['Leer un texto y resumir el contenido.'
+        , 'Memorizar un vocabulario.', 'Escribir un texto corto.'
+        , 'Todas las respuestas anteriores son correctas.']
+    },
+    {
+        numb: 13,
+        question: "Qué sugerencias no debemos facilitar a las usuarias para favorecer la memoria: ",
+        answer: "Cambiar las cosas de sitio con frecuencia para fortalecer la memoria.",
+        options: ['Ordenar las cosas, clasificándolas, para encontrarlas con mayor facilidad.', 
+        'Cambiar las cosas de sitio con frecuencia para fortalecer la memoria.',
+            'Colgar notas en lugares donde no queramos olvidar algo importante.', 
+            'Todas las respuestas anteriores son correctas.']
+    },
+    {
+        numb: 14,
+        question: "Para trabajar la orientación espacial con Martina utilizarás: ",
+        answer: "Contar los pasos de un lugar a otro.",
+        options: ['Contar los pasos de un lugar a otro.'
+        , 'Trabajar los horarios del día.', 
+        'Decir el nombre de sus hijos.', 
+        'Hacerle preguntas sobre su pasado.']
+    },
+    {
+        numb: 15,
+        question: "¿Qué características presentan las personas mayores en relación al lenguaje?",
+        answer: "No cambia de demasiado a lo largo de la vida.",
+        options: ['Cambia mucho a medida que nos hacemos mayores.', 'Cambia hasta la adolescencia y luego se mantiene igual el resto de la vida.'
+        , 'Cambia continuamente, incluso cuando somos mayores.', 
+        'No cambia de demasiado a lo largo de la vida.']
+    },
+];
